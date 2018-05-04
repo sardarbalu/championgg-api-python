c8513a6c28fe74720ced74d44b515bf9c97bd426
sardarbalu <34949744+sardarbalu@users.noreply.github.com>
2018-05-04 14:53:56 -0700

    changing to options

--- py_gg/champions.py
+++ py_gg/champions.py
@@ -18,7 +18,7 @@ class Champions(object):
         if options is None:
             options = {}
         if self.__alldata == "":
-            r = requests.get("%s/champions?api_key=%s&%s" % (self.__url, self.__key, urlencode(options)))
+            r = requests.get("%s/champions?api_key=%s&%s" % (self.__url, self.__key, options))
             self.__alldata = r.json()
         return self.__alldata
 
@@ -27,7 +27,7 @@ class Champions(object):
             options = {}
         if not isinstance(id, int):
             raise _InvalidChampionIdError("Champion Id must be present and an integer.")
-        r = requests.get("%s/champions/%d?api_key=%s&%s" % (self.__url, id, self.__key, urlencode(options)))
+        r = requests.get("%s/champions/%d?api_key=%s&%s" % (self.__url, id, self.__key, options))
         return r.json()
 
     def specific_role(self, id, role, options=None):
@@ -37,7 +37,7 @@ class Champions(object):
             raise _InvalidChampionIdError("Champion Id must be present and an integer.")
         if role not in ['TOP', 'JUNGLE', 'MIDDLE', 'DUO_CARRY', 'DUO_SUPPORT']:
             raise _InvalidRoleError("Role must be present and either be TOP, JUNGLE, MIDDLE, DUO_CARRY, or DUO_SUPPORT.")
-        r = requests.get("%s/champions/%d/%s?api_key=%s&%s" % (self.__url, id, role, self.__key, urlencode(options)))
+        r = requests.get("%s/champions/%d/%s?api_key=%s&%s" % (self.__url, id, role, self.__key, options))
         try:
             return r.json()[0]
         except IndexError:
@@ -53,5 +53,5 @@ class Champions(object):
         if role not in ['TOP', 'JUNGLE', 'MIDDLE', 'DUO_CARRY', 'DUO_SUPPORT']:
             raise _InvalidRoleError("Role must be present and either be TOP, JUNGLE, MIDDLE, DUO_CARRY, or DUO_SUPPORT.")
         r = requests.get("%s/champions/%d/matchups/%d/%s?api_key=%s&%s" %
-                         (self.__url, champ1, champ2, role, self.__key, urlencode(options)))
+                         (self.__url, champ1, champ2, role, self.__key, options))
         return r.json()
